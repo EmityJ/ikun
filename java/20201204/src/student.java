7764e05cd4e18c209e663f83cf3484fa3555eda6
EmityJ <y806294605@qq.com>
2020-12-04 14:50:27 +0800

    第一个extends，为什么底下的shuchu函数要弄成static类型的

---
+++ java/20201204/src/student.java
@@ -0,0 +1,11 @@
+public class student extends person {
+    public static void main(String[] args) {
+    shuchu ();
+    }
+}
+class person{
+    static void shuchu(){
+        System.out.println("输出这句话");
+    }
+
+}
